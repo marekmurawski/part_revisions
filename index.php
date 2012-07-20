a0b597c0513e719756903cc02f07936e14518fc4
marekmurawski <marek.murawski@gmail.com>
2012-07-20 14:07:59 +0200

    Version change - 0.0.4

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ Plugin::setInfos(array(
 		'id' => 'part_revisions',
 		'title' => __('Page Part Revisions'),
 		'description' => __('Provides Page Part revisions history management.'),
-		'version' => '0.0.3',
+		'version' => '0.0.4',
 		'license' => 'GPL',
 		'author' => 'Marek Murawski',
 		'website' => 'http://www.marekmurawski.pl/',
