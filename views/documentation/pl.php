3ead73bb3ac4278e5d3d536d188c804fe673bdb9
marekmurawski <marek.murawski@gmail.com>
2012-07-20 14:04:38 +0200

    Polish Translation

---
+++ views/documentation/pl.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('IN_CMS')) {
+	exit();
+}
+?>
+
+<h1>Dokumentacja</h1>
+
+<h2>Rewizje Sekcji</h2>
+<div>
+	<p>
+		Plugin Rewizje Sekcji pozwala zachować zmiany i wersje Sekcji Stron. Dzięki niemu
+		możesz śledzić i przywracać zmiany w Sekcjach wprowadzane przez użytkowników.
+	</p>
+	<p>
+		Po każdej edycji Sekcji zachowuje on poprzednią wersję, którą możesz powrócić w przyszłości.
+	</p>
+
+</div>
